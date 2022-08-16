f1cbb5652a5f2ce784ef6aa9e50df6b5a8fecddd
RAJBOKADE <raj.bokade1999@gmail.com>
2022-08-16 15:47:36 +0530

    new files are added

---
+++ src/com/crossasyst/test/Demo.java
@@ -0,0 +1,13 @@
+package com.crossasyst.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo {
+    public static final Logger LOG = LogManager.getLogger(Demo.class);
+
+
+    public static void main(String[] args) {
+        LOG.info("My Name is Raj");
+    }
+}
